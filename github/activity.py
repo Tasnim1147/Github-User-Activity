99bc20fa67cfe350c7c0676bbf99fc36fda07b12
Tasnim1147 <87118828+Tasnim1147@users.noreply.github.com>
2024-12-25 03:39:55 +0000

    Implement Activity

--- github/activity.py
+++ github/activity.py
@@ -1,3 +1,4 @@
+from typing import Union
 import requests, json, time
 
 from functools import lru_cache
@@ -16,17 +17,35 @@ class Activity(object):
         self.username = username
         self.events = None
         self.needed_time = 0
+        self.activities = None
+        self.response = None
         self._process_events()
 
-    def _process_events(self): pass
+    def _process_events(self): 
+        start_time = time.time()
+        self.response = get_acitivities(self.username)
+        self.activities = json.loads(self.response)
+        end_time = time.time()
+        self.needed_time = end_time - start_time
 
-    def is_valid_response(self): pass
+    def is_valid_response(self): 
+        return self.response.status_code
 
     def get_activities(self,
                        activity_types: list=[]
-                       ) -> list[dict[str, str]]:
-        pass
+                       ) -> list[dict[str, Union[str, bool]]]:
+        filtered_activities = filter(lambda activity: activity in activity_types,
+                                     self.activities)
+        activities = list(map(lambda activity: {
+            'type': activity['type'],
+            'id': activity['id'],
+            'created_at': activity['created_at'],
+            'public': activity['public'],
+            'repo_name': activity['repo']['name']
+        }))
+        return activities
 
-    def get_types_of_events(self): pass
+    def get_types_of_events(self): 
+        return set(map(lambda activity: activity['type'], self.activities))
 
-    def get_needed_time(self) -> int: pass
+    def get_needed_time(self) -> int: return self.needed_time
